c0fea06fdb515d444272bf6e47a4c9a0d2efa4fe
ArseneLupinhb <hongboal@qq.com>
2020-04-13 12:55:10 +0800

    [add] np.reshape

--- question/others.py
+++ question/others.py
@@ -1,5 +1,13 @@
+# 打乱列表
 import random
 
 list_data = [20, 16, 10, 5];
 random.shuffle(list_data)
 print("随机排序列表 : ", list_data)
+
+# np.reshape
+import numpy as np
+
+np_data = np.arange(12)
+print(np_data)
+np.reshape(np_data, [3, 4])
