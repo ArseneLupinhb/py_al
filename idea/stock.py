5c062aa953814da9a8ae5863b6b12e3072d75b14
ArseneLupinhb <hongboal@qq.com>
2020-04-25 10:32:46 +0800

    [update] 解决保存csv文件中文乱码问题

--- idea/stock.py
+++ idea/stock.py
@@ -1,6 +1,17 @@
+import os
+
 import tushare as ts
 
+# 全局路径设置
+os.getcwd()
+source_path = os.getcwd() + r'/idea/work/'
+
 # 一次性获取最近一个日交易日所有股票的交易数据
-ts.get_today_all()
+ts_df = ts.get_today_all()
 # 新闻数据
-ts.get_notices
+new_df = ts.get_notices
+
+ts_df.info()
+ts_df.head()
+# 使用 utf_8_sig 可以解决中文乱码问题
+ts_df.to_csv(source_path + r'ts_df.csv', encoding='utf_8_sig')
