7814d07c8bfa9005c20542f562d38286490e40a9
ArseneLupinhb <hongboal@qq.com>
2020-04-25 09:42:14 +0800

    [update] 修改函数名 和 参数

--- visualization/text_cloud.py
+++ visualization/text_cloud.py
@@ -1,9 +1,15 @@
-from pyecharts import WordCloud
+from pyecharts.charts import WordCloud
 
 
-def get_text_cloud(name, value, generate_path):
-	wordcloud = WordCloud(width=1000, height=620)
-	wordcloud.add("", name, value, word_size_range=[20, 80])
-	wordcloud.render(generate_path)
-	# wordcloud.render(r'D:\source\repos_branch_2\Algorithms\Python-master\test\word.html')
-	wordcloud
+def get_text_cloud_by_label_value(name, value, generate_path):
+    wordcloud = WordCloud(width=1000, height=620)
+    wordcloud.add("", name, value, word_size_range=[20, 80])
+    wordcloud.render(generate_path)
+    wordcloud
+
+
+def get_text_cloud_by_data(data, generate_path):
+    wordcloud = WordCloud()
+    wordcloud.add("", data, word_size_range=[20, 80])
+    wordcloud.render(generate_path)
+    wordcloud
