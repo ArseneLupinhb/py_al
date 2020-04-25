43b8e493d4cb910d7933e70eee83cb9bae625cf6
ArseneLupinhb <hongboal@qq.com>
2020-04-26 01:35:19 +0800

    [update] 修改文件名 删除 多余文件
    Signed-off-by: ArseneLupinhb <hongboal@qq.com>

--- learn/data_analysis/normal_data_analysis/text_cloud.py
+++
@@ -1,9 +0,0 @@
-from pyecharts.charts import WordCloud
-
-
-def get_text_cloud(name, value, path):
-    wordcloud = WordCloud(width=1000, height=620)
-    wordcloud.add("", name, value, word_size_range=[20, 80])
-    generatepath = path
-    wordcloud.render(generatepath)
-    wordcloud
