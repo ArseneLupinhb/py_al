2a45a506de2f005ad7935d112d63e1c151561dcc
ArseneLupinhb <hongboal@qq.com>
2020-05-22 01:08:02 +0800

    [add] 添加paddle nlp 通过 id 将文本转换成数字
    Signed-off-by: ArseneLupinhb <hongboal@qq.com>

---
+++ learn/paddle_learn/nlp/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: al
+@file: __init__.py.py
+@time: 2020/5/21 23:45
+@desc:
+'''
---
+++ learn/paddle_learn/nlp/word_tensor.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: al
+@file: word_tensor.py
+@time: 2020/5/22 0:46
+@desc:
+'''
+import math
+import os
+import random
+
+import requests
+
+
+def download():
+    # 可以从百度云服务器下载一些开源数据集（dataset.bj.bcebos.com）
+    corpus_url = "https://dataset.bj.bcebos.com/word2vec/text8.txt"
+    # 使用python的requests包下载数据集到本地
+    web_request = requests.get(corpus_url)
+    corpus = web_request.content
+    # 把下载后的文件存储在当前目录的text8.txt文件内
+    with open("work/text8.txt", "wb") as f:
+        f.write(corpus)
+    f.close()
+
+
+# 读取text8数据
+def load_text8():
+    with open("work/text8.txt", "r") as f:
+        corpus = f.read().strip("\n")
+    f.close()
+    return corpus
+
+
+# 对语料进行预处理（分词）
+def data_preprocess(corpus):
+    # 由于英文单词出现在句首的时候经常要大写，所以我们把所有英文字符都转换为小写，
+    # 以便对语料进行归一化处理（Apple vs apple等）
+    corpus = corpus.strip().lower()
+    corpus = corpus.split(" ")
+
+    return corpus
+
+
+# 构造词典，统计每个词的频率，并根据频率将每个词转换为一个整数id
+def build_dict(corpus):
+    # 首先统计每个不同词的频率（出现的次数），使用一个词典记录
+    word_freq_dict = dict()
+    for word in corpus:
+        if word not in word_freq_dict:
+            word_freq_dict[word] = 0
+        word_freq_dict[word] += 1
+
+    # 将这个词典中的词，按照出现次数排序，出现次数越高，排序越靠前
+    # 一般来说，出现频率高的高频词往往是：I，the，you这种代词，而出现频率低的词，往往是一些名词，如：nlp
+    word_freq_dict = sorted(word_freq_dict.items(), key=lambda x: x[1], reverse=True)
+
+    # 构造3个不同的词典，分别存储，
+    # 每个词到id的映射关系：word2id_dict
+    # 每个id出现的频率：word2id_freq
+    # 每个id到词典映射关系：id2word_dict
+    word2id_dict = dict()
+    word2id_freq = dict()
+    id2word_dict = dict()
+
+    # 按照频率，从高到低，开始遍历每个单词，并为这个单词构造一个独一无二的id
+    for word, freq in word_freq_dict:
+        curr_id = len(word2id_dict)
+        word2id_dict[word] = curr_id
+        word2id_freq[word2id_dict[word]] = freq
+        id2word_dict[curr_id] = word
+
+    return word2id_freq, word2id_dict, id2word_dict
+
+
+# 把语料转换为id序列
+def convert_corpus_to_id(corpus, word2id_dict):
+    # 使用一个循环，将语料中的每个词替换成对应的id，以便于神经网络进行处理
+    corpus = [word2id_dict[word] for word in corpus]
+    return corpus
+
+
+# 使用二次采样算法（subsampling）处理语料，强化训练效果
+def subsampling(corpus, word2id_freq):
+    # 这个discard函数决定了一个词会不会被替换，这个函数是具有随机性的，每次调用结果不同
+    # 如果一个词的频率很大，那么它被遗弃的概率就很大
+    def discard(word_id):
+        return random.uniform(0, 1) < 1 - math.sqrt(
+            1e-4 / word2id_freq[word_id] * len(corpus))
+
+    corpus = [word for word in corpus if not discard(word)]
+    return corpus
+
+
+# 构造数据，准备模型训练
+# max_window_size代表了最大的window_size的大小，程序会根据max_window_size从左到右扫描整个语料
+# negative_sample_num代表了对于每个正样本，我们需要随机采样多少负样本用于训练，
+# 一般来说，negative_sample_num的值越大，训练效果越稳定，但是训练速度越慢。
+def build_data(corpus, word2id_dict, word2id_freq, max_window_size=3, negative_sample_num=4):
+    # 使用一个list存储处理好的数据
+    dataset = []
+
+    # 从左到右，开始枚举每个中心点的位置
+    for center_word_idx in range(len(corpus)):
+        # 以max_window_size为上限，随机采样一个window_size，这样会使得训练更加稳定
+        window_size = random.randint(1, max_window_size)
+        # 当前的中心词就是center_word_idx所指向的词
+        center_word = corpus[center_word_idx]
+
+        # 以当前中心词为中心，左右两侧在window_size内的词都可以看成是正样本
+        positive_word_range = (
+            max(0, center_word_idx - window_size), min(len(corpus) - 1, center_word_idx + window_size))
+        positive_word_candidates = [corpus[idx] for idx in range(positive_word_range[0], positive_word_range[1] + 1) if
+                                    idx != center_word_idx]
+
+        # 对于每个正样本来说，随机采样negative_sample_num个负样本，用于训练
+        for positive_word in positive_word_candidates:
+            # 首先把（中心词，正样本，label=1）的三元组数据放入dataset中，
+            # 这里label=1表示这个样本是个正样本
+            dataset.append((center_word, positive_word, 1))
+
+            # 开始负采样
+            i = 0
+            while i < negative_sample_num:
+                negative_word_candidate = random.randint(0, vocab_size - 1)
+
+                if negative_word_candidate not in positive_word_candidates:
+                    # 把（中心词，正样本，label=0）的三元组数据放入dataset中，
+                    # 这里label=0表示这个样本是个负样本
+                    dataset.append((center_word, negative_word_candidate, 0))
+                    i += 1
+
+    return dataset
+
+
+if __name__ == '__main__':
+    os.chdir(r'D:\soft_own\source\py_al\learn\paddle_learn\nlp')
+    download()
+    corpus = load_text8()
+    print(corpus[:500])
+    corpus = data_preprocess(corpus)
+    print(corpus[:50])
+
+    word2id_freq, word2id_dict, id2word_dict = build_dict(corpus)
+    vocab_size = len(word2id_freq)
+    print("there are totoally %d different words in the corpus" % vocab_size)
+    for _, (word, word_id) in zip(range(50), word2id_dict.items()):
+        print("word %s, its id %d, its word freq %d" % (word, word_id, word2id_freq[word_id]))
+
+    # 就这样把文本转换成数字了哈哈哈
+    corpus = convert_corpus_to_id(corpus, word2id_dict)
+    print("%d tokens in the corpus" % len(corpus))
+    print(corpus[:50])
+
+    corpus = subsampling(corpus, word2id_freq)
+    print("%d tokens in the corpus" % len(corpus))
+    print(corpus[:50])
+
+    # stop for the 内存
+    # 但是如果是在公司这样的一种环境熟完全不能跑这样的代码的。
+    dataset = build_data(corpus, word2id_dict, word2id_freq)
+    for _, (center_word, target_word, label) in zip(range(50), dataset):
+        print("center_word %s, target %s, label %d" % (id2word_dict[center_word],
+                                                       id2word_dict[target_word], label))
