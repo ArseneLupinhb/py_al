1e5b2ab329454099281d578c0782448154586419
ArseneLupinhb <hongboal@qq.com>
2020-05-26 01:41:52 +0800

    [add] 添加line
    Signed-off-by: ArseneLupinhb <hongboal@qq.com>

--- learn/data_analysis/pye_sub/pye_utils.py
+++ learn/data_analysis/pye_sub/pye_utils.py
@@ -11,6 +11,8 @@ import os
 
 from pyecharts import options as opts
 from pyecharts.charts import Bar
+from pyecharts.charts import Line
+from pyecharts.charts import Map
 from pyecharts.globals import CurrentConfig
 from pyecharts.globals import ThemeType
 from pyecharts.render import make_snapshot
@@ -37,6 +39,23 @@ def pye_bar_phoot(x_data, y_data, ylabel):
     make_snapshot(snapshot, bar.render(), "bar.png")
 
 
+def get_line():
+    lin = (
+        Line(init_opts=opts.InitOpts(theme=ThemeType.CHALK))
+            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90], is_smooth=True)
+            .add_yaxis("商家B", [15, 6, 45, 20, 35, 66], is_smooth=True)
+            .set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"),
+                             xaxis_opts=opts.AxisOpts(name='日期'),
+                             yaxis_opts=opts.AxisOpts(name='数量', max_=16000, min_=1, type_="log",  # 坐标轴配置项
+                                                      splitline_opts=opts.SplitLineOpts(is_show=True),  # 分割线配置项
+                                                      axisline_opts=opts.AxisLineOpts(is_show=True))
+                             )
+
+    )
+    lin.render()
+
+
 if __name__ == '__main__':
     os.chdir(r'D:\soft_own\source\py_al\learn\data_analysis\pye_sub')
     os.getcwd()
@@ -49,10 +68,18 @@ if __name__ == '__main__':
 
     # bar demo2
     bar = (
-        Bar(init_opts=opts.InitOpts(theme=ThemeType.DARK))
+        Bar(init_opts=opts.InitOpts(theme=ThemeType.CHALK))
             .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
-            .add_yaxis("商家B", [15, 6, 45, 20, 35, 66])
+            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90], is_smooth=True)
+            .add_yaxis("商家B", [15, 6, 45, 20, 35, 66], is_smooth=True)
             .set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"))
     )
     bar.render()
+
+    get_line()
+
+    value = [95.1, 23.2, 43.3, 66.4, 88.5]
+    attr = ["China", "Canada", "Brazil", "Russia", "United States"]
+    map = Map("世界地图示例")
+    map.add("", attr, value, maptype="world", is_visualmap=True, visual_text_color='#000')
+    map.render('Map-World.html')
